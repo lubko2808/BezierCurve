42d22e1f7389446c7a3677c36971b3176c06a856
Lubomyr Chorniak <lubko2802@gmail.com>
2024-03-05 17:46:55 +0200

    Initial Commit

---
+++ CG_Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CG_Lab2
+//
+//  Created by Lubomyr Chorniak on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
